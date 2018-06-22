024827f3f4ab5bef07214014705263a179bd919d
leavesC <199072443@qq.com>
2018-06-22 23:12:57 +0800

    add sourceCode

---
+++ Java_Android_Learn/sourceCode/HandlerThread.java
@@ -0,0 +1,120 @@
+package android.os;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+
+/**
+ * Handy class for starting a new thread that has a looper. The looper can then be 
+ * used to create handler classes. Note that start() must still be called.
+ */
+public class HandlerThread extends Thread {
+
+    //线程优先级
+    int mPriority;
+
+    //线程ID
+    int mTid = -1;
+
+    //当前线程持有的Looper对象
+    Looper mLooper;
+
+    //包含当前 Looper 对象的 Handler
+    private @Nullable Handler mHandler;
+
+    public HandlerThread(String name) {
+        super(name);
+        //使用默认的线程优先级
+        mPriority = Process.THREAD_PRIORITY_DEFAULT;
+    }
+
+    public HandlerThread(String name, int priority) {
+        super(name);
+        //使用自定义的线程优先级
+        mPriority = priority;
+    }
+    
+    //在 Looper 循环启动前调用
+    //此处是空实现，留待子类重写
+    protected void onLooperPrepared() {
+    }
+
+    @Override
+    public void run() {
+        mTid = Process.myTid();
+        //触发当前线程创建 Looper 对象
+        Looper.prepare();
+        synchronized (this) {
+            //获取 Looper 对象
+            mLooper = Looper.myLooper();
+            //唤醒在等待的线程
+            //唤醒 getLooer() 中可能还处于等待状态的线程
+            notifyAll();
+        }
+        //设置线程优先级
+        Process.setThreadPriority(mPriority);
+        onLooperPrepared();
+        //开启消息循环
+        Looper.loop();
+        mTid = -1;
+    }
+    
+    //获取与当前线程关联的 Looper 对象
+    //因为 getLooper() 方法可能先于 run() 被调用，此时就需要先等待 Looper 对象被创建
+    public Looper getLooper() {
+        //如果当前线程未在运行，则返回 null 
+        if (!isAlive()) {
+            return null;
+        }
+        synchronized (this) {
+            //如果当前线程已处理运行状态（已调用 start() 方法）且 Looper 对象还未创建
+            //则调用 wait() 方法释放锁，等待 Looper 对象创建
+            while (isAlive() && mLooper == null) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+        return mLooper;
+    }
+
+    /**
+     * @return a shared {@link Handler} associated with this thread
+     * @hide
+     */
+    @NonNull
+    public Handler getThreadHandler() {
+        if (mHandler == null) {
+            mHandler = new Handler(getLooper());
+        }
+        return mHandler;
+    }
+
+    //清空消息队列中所有的消息
+    public boolean quit() {
+        Looper looper = getLooper();
+        if (looper != null) {
+            looper.quit();
+            return true;
+        }
+        return false;
+    }
+
+    //清空消息队列中所有非延时消息
+    public boolean quitSafely() {
+        Looper looper = getLooper();
+        if (looper != null) {
+            looper.quitSafely();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the identifier of this thread. See Process.myTid().
+     */
+    public int getThreadId() {
+        return mTid;
+    }
+
+}
---
+++ Java_Android_Learn/sourceCode/IntentService.java
@@ -0,0 +1,112 @@
+package android.app;
+
+import android.annotation.WorkerThread;
+import android.annotation.Nullable;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+
+/**
+ * 作者：叶应是叶
+ * 时间：2018/6/22 13:39
+ * 描述：https://github.com/leavesC
+ * https://www.jianshu.com/u/9df45b87cfdf
+ */
+public abstract class IntentService extends Service {
+
+    private volatile Looper mServiceLooper;
+
+    //关联了 mServiceLooper 的 Handler
+    private volatile ServiceHandler mServiceHandler;
+
+    //IntentService 内部使用的子线程的线程名
+    private String mName;
+
+    //用于决定 onStartCommand() 方法的返回值
+    private boolean mRedelivery;
+
+    private final class ServiceHandler extends Handler {
+
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            //将耗时任务转交给 onHandleIntent() 方法来完成
+            //onHandleIntent() 需要由子类来实现
+            onHandleIntent((Intent)msg.obj);
+            //msg.arg1 即 startId
+            //如果当前
+            stopSelf(msg.arg1);
+        }
+    }
+
+    //传入子线程名
+    public IntentService(String name) {
+        super();
+        mName = name;
+    }
+
+    //用于决定 onStartCommand() 方法的返回值
+    //如果参数为 true 则返回 START_REDELIVER_INTENT
+    //如果系统在 onStartCommand() 返回后终止服务，则会重建服务，并通过传递给服务的最后一个 Intent 调用 onStartCommand()
+    //任何挂起 Intent 均依次传递。这适用于主动执行应该立即恢复的作业（例如下载文件）的服务
+    //如果参数为 false 则返回 START_NOT_STICKY
+    //如果系统在 onStartCommand() 返回后终止服务，则除非有挂起 Intent 要传递，否则系统不会重建服务
+    //这是最安全的选项，可以避免在不必要时以及应用能够轻松重启所有未完成的作业时运行服务
+    public void setIntentRedelivery(boolean enabled) {
+        mRedelivery = enabled;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        HandlerThread thread = new HandlerThread("IntentService[" + mName + "]");
+        //触发 HandlerThread 创建 Looper 对象
+        thread.start();
+        //获取 Looper 对象，以此来构建可以向子线程发送 Message 的 Handler
+        mServiceLooper = thread.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+    }
+
+    @Override
+    public void onStart(@Nullable Intent intent, int startId) {
+        Message msg = mServiceHandler.obtainMessage();
+        msg.arg1 = startId;
+        msg.obj = intent;
+        mServiceHandler.sendMessage(msg);
+    }
+
+    //每次回调 onStartCommand() 方法时，参数 startId 的值都是自动递增的，startId 用于唯一标识每次对 Service 发起的处理请求
+    //如果 Service 同时处理多个 onStartCommand() 请求，则不应在处理完一个启动请求之后立即销毁 Service
+    //因为此时可能已经收到了新的启动请求，在第一个请求结束时停止服务会导致第二个请求被终止
+    //为了避免这一问题，可以使用 stopSelf(int) 确保 Service 停止请求始终是基于最新一次的启动请求
+    //也就是说，如果调用 stopSelf(int) 方法的参数值与 onStartCommand() 接受到的最新的 startId 值不相符的话，stopSelf(int) 方法就会失效，从而避免终止尚未处理的请求
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        onStart(intent, startId);
+        return mRedelivery ? START_REDELIVER_INTENT : START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        //移除所有待发送的 Message
+        mServiceLooper.quit();
+    }
+
+    @Override
+    @Nullable
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    //此方法需要由子类来实现
+    //在此方式中完成具体的耗时任务
+    @WorkerThread
+    protected abstract void onHandleIntent(@Nullable Intent intent);
+
+}
